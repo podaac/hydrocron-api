69f7f318ff4c4145953d1b239f09cba6212a1c05
vggonzal <9Tcostoamm>
2023-08-05 18:29:48 -0700

    update terraform to provision endpoints - change docker script

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,8 @@
 variable "app_name" {default = "hydrocron-api"}
 variable "db_app_name" {default = "hydrocron-db"}
 variable "stage" {}
-variable "credentials" {}
-variable "profile" { }
 variable "vpc_id" {}
 variable "private_subnets" {}
-variable "docker_tag" {}
 variable "default_vpc_sg" {}
 
 variable "default_tags" {
