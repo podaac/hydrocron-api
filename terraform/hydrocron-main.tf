10bc29a883751d841f9b7cbe7d054dfb787dfafb
vggonzal <9Tcostoamm>
2023-08-06 16:07:02 -0700

    implementing the lambda function

--- terraform/hydrocron-main.tf
+++ terraform/hydrocron-main.tf
@@ -55,7 +55,132 @@ resource "aws_security_group_rule" "allow_app_in" {
   to_port     = 3306
   source_security_group_id = aws_security_group.service-app-sg.id
 }
+/*
+# Lambda Function for the last stable pre-1.0 release of the API. This function is intended to be temprorary
+# and should be removed once clients have moved off of this version (primarily, earthdata search client)
+resource "aws_lambda_function" "hydrocron_api_lambda_0_0_1" {
+  function_name = "${local.ec2_resources_name}-0_0_1"
+  role          = aws_iam_role.hydrocron-service-role.arn
+  package_type = "Image"
+  image_uri     = "ghcr.io/podaac/hydrocron-api:0.0.1"
+  timeout       = 5
 
+  vpc_config {
+    subnet_ids = var.private_subnets
+    security_group_ids = [aws_security_group.service-app-sg.id]
+  }
+
+  environment {
+    variables = {
+      DB_HOST=data.aws_ssm_parameter.hydrocron-db-host.value
+      DB_NAME=data.aws_ssm_parameter.hydrocron-db-name.value
+      DB_USERNAME=data.aws_ssm_parameter.hydrocron-db-user.value
+      DB_PASSWORD_SSM_NAME=data.aws_ssm_parameter.hydrocron-db-user-pass.name
+    }
+  }
+
+  tags = merge(local.default_tags, {
+        "Version": "0.0.1"
+    })
+}
+
+resource "aws_lambda_permission" "allow_hydrocron_0_0_1" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hydrocron_api_lambda_0_0_1.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.hydrocron-api-gateway.execution_arn}/*/*/*"
+}
+
+resource "aws_api_gateway_deployment" "hydrocron-api-gateway-deployment" {
+  rest_api_id = aws_api_gateway_rest_api.hydrocron-api-gateway.id
+  stage_name  = "default"
+  depends_on = [aws_api_gateway_rest_api.hydrocron-api-gateway]
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_rest_api.hydrocron-api-gateway.body
+    ]))
+  }
+}
+
+resource "aws_lambda_function" "hydrocron_api_lambdav1" {
+  function_name = "${local.ec2_resources_name}-function"
+  role          = aws_iam_role.hydrocron-service-role.arn
+  package_type = "Image"
+  image_uri     = "ghcr.io/podaac/hydrocron-api:0.0.1"
+  timeout       = 5
+
+  vpc_config {
+    subnet_ids = var.private_subnets
+    security_group_ids = [aws_security_group.service-app-sg.id]
+  }
+
+  environment {
+    variables = {
+      DB_HOST=data.aws_ssm_parameter.hydrocron-db-host.value
+      DB_NAME=data.aws_ssm_parameter.hydrocron-db-name.value
+      DB_USERNAME=data.aws_ssm_parameter.hydrocron-db-user.value
+      DB_PASSWORD_SSM_NAME=data.aws_ssm_parameter.hydrocron-db-user-pass.name
+    }
+  }
+
+  tags = var.default_tags
+}
+
+resource "aws_lambda_permission" "allow_hydrocron" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hydrocron_api_lambdav1.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  # The "/*/*/*" portion grants access from any method on any resource
+  # within the API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.hydrocron-api-gateway.execution_arn}/*/*/*"
+}
+
+
+
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "hydrocron-api-gateway" {
+  name        = "${local.ec2_resources_name}-api-gateway"
+  description = "API to access Hydrocron"
+  body        = templatefile(
+                  "${path.module}/api-specification-templates/hydrocron_aws_api.yml",
+                  {
+                    hydrocronapi_v001_lambda_arn = aws_lambda_function.hydrocron_api_lambda_0_0_1.invoke_arn
+                    hydrocronapi_lambda_arn = aws_lambda_function.hydrocron_api_lambdav1.invoke_arn
+                    vpc_id = var.vpc_id
+                  })
+  parameters = {
+    "basemap" = "split"
+  }
+  endpoint_configuration {
+    types = ["PRIVATE"]
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_cloudwatch_log_group" "hydrocron-api-gateway-logs" {
+  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.hydrocron-api-gateway.id}/${aws_api_gateway_deployment.hydrocron-api-gateway-deployment.stage_name}"
+  retention_in_days = 60
+}
+
+output "url" {
+  value = "${aws_api_gateway_deployment.hydrocron-api-gateway-deployment.invoke_url}/api"
+}
+
+resource "aws_ssm_parameter" "hydrocron-api-url" {
+  name  = "hydrocron-api-url"
+  type  = "String"
+  value = aws_api_gateway_deployment.hydrocron-api-gateway-deployment.invoke_url
+}
+*/
 #########################
 # CodeBuild functionality
 #########################
