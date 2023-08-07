dc4e9639664d58eb230b0d4fc8bc4c9f4abc836b
vggonzal <9Tcostoamm>
2023-08-07 16:28:48 -0700

    implementing the lambda function

--- terraform/bin/config.sh
+++ terraform/bin/config.sh
@@ -39,6 +39,33 @@ esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
+
+
+
+PACKAGE_NAME=$(awk -F' = ' '{gsub(/"/,"");if($1=="name")print $2}' pyproject.toml)
+VERSION=$(poetry version -s)
+
+ROOT_PATH="$PWD"
+ZIP_PATH="$ROOT_PATH/dist/$PACKAGE_NAME-$VERSION.zip"
+
+# Install the bundle plugin using `poetry self add poetry-plugin-bundle`
+poetry bundle venv build --clear --without=dev
+
+cd build/lib/python3.*/site-packages
+touch hydrocronapi/__init__.py
+rm -rf *.dist-info _virtualenv.*
+find . -type d -name __pycache__ -exec rm -rf {} \+
+
+mkdir -p "$ROOT_PATH/dist/"
+rm -f "$ZIP_PATH"
+zip -vr9 "$ZIP_PATH" .
+
+
+
+
+
+
+
 # https://www.terraform.io/docs/commands/environment-variables.html#tf_in_automation
 TF_IN_AUTOMATION=true
 
