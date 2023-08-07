635142ef5930fd9990b19d6551f7c61e083d2d58
vggonzal <9Tcostoamm>
2023-08-07 11:27:33 -0700

    implementing the lambda function

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,19 +3,9 @@ resource "aws_s3_bucket" "internal" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "internal-acl" {
-   bucket = aws_s3_bucket.internal.id
-   acl = "private"
-}
-
 resource "aws_s3_bucket" "public" {
    bucket = "${local.ec2_resources_name}-public"
   force_destroy = true
-}
-
-resource "aws_s3_bucket_acl" "public-acl" {
-   bucket = aws_s3_bucket.public.id
-   acl = "private"
 }
 
 resource "aws_s3_bucket" "private" {
@@ -23,17 +13,7 @@ resource "aws_s3_bucket" "private" {
   force_destroy = true
 }
 
-resource "aws_s3_bucket_acl" "private-acl" {
-   bucket = aws_s3_bucket.private.id
-   acl = "private"
-}
-
 resource "aws_s3_bucket" "protected" {
    bucket = "${local.ec2_resources_name}-protected"
   force_destroy = true
-}
-
-resource "aws_s3_bucket_acl" "protected-acl" {
-   bucket = aws_s3_bucket.protected.id
-   acl = "private"
 }
