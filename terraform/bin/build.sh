bead4fc750c0b3ad01cd1ae43783a2df75dae00b
vggonzal <9Tcostoamm>
2023-08-07 16:38:33 -0700

    implementing the lambda function

--- terraform/bin/build.sh
+++ terraform/bin/build.sh
@@ -10,3 +10,5 @@ ZIP_PATH="$ROOT_PATH/dist/$PACKAGE_NAME-$VERSION.zip"
 mkdir -p "$ROOT_PATH/dist/"
 rm -f "$ZIP_PATH"
 zip -vr9 "$ZIP_PATH" .
+echo "++++"
+echo "$ROOT_PATH/dist/$PACKAGE_NAME-$VERSION.zip"
