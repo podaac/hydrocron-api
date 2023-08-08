af19e977b06bda67c18c0b5a1c00ac34662e248c
vggonzal <9Tcostoamm>
2023-08-07 18:37:51 -0700

    implementing the lambda function

---
+++ terraform/hydrocron-lambda-iam.tf
@@ -0,0 +1,21 @@
+
+resource "aws_iam_role" "hydrocron-service-role" {
+  name = "${local.ec2_resources_name}-service-role"
+
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/NGAPShRoleBoundary"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
