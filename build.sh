5c6adb8b7421b63964c75be4e7de66ff25620a6c
vggonzal <9Tcostoamm>
2023-08-07 19:22:52 -0700

    implementing the lambda function

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@ PACKAGE_NAME=$(awk -F' = ' '{gsub(/"/,"");if($1=="name")print $2}' pyproject.tom
 VERSION="0_0_1"
 ROOT_PATH="$PWD"
 ZIP_PATH="$ROOT_PATH/dist/$PACKAGE_NAME-$VERSION.zip"
+echo "----"
+echo $ZIP_PATH
 
 mkdir -p "$ROOT_PATH/dist/"
 rm -f "$ZIP_PATH"
