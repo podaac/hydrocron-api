79a6c12af59adaba1c5b5bed0631b0f73f08fc89
vggonzal <9Tcostoamm>
2023-08-09 16:32:22 -0700

    implementing the lambda function

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eo pipefail
+
+PACKAGE_NAME=$(awk -F' = ' '{gsub(/"/,"");if($1=="name")print $2}' pyproject.toml)
+VERSION="0_0_1"
+ROOT_PATH="$PWD"
+ZIP_PATH="$ROOT_PATH/dist/$PACKAGE_NAME-$VERSION.zip"
+mkdir -p "$ROOT_PATH/dist/"
+rm -f "$ZIP_PATH"
+zip -vr9 "$ZIP_PATH" .
