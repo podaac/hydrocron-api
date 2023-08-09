ae2e082f52e99df48ffaf03d22abf6fb79f2edbe
vggonzal <9Tcostoamm>
2023-08-09 16:15:15 -0700

    implementing the lambda function

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,3 +30,47 @@ locals {
 }
 
 data "aws_caller_identity" "current" {}
+
+
+module "dynamodb_table" {
+  source = "../../"
+
+  name                        = "Hydrocron_db"
+  hash_key                    = "id"
+  range_key                   = "feature_id"
+  table_class                 = "STANDARD"
+  deletion_protection_enabled = false
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    },
+    {
+      name = "feature_id"
+      type = "S"
+    }
+  ]
+
+  global_secondary_indexes = [
+    {
+      name               = "Hydrocron_db"
+      hash_key           = "Hydrocron_db"
+      range_key          = "feature_id"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["id"]
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+}
+
+
+module "disabled_dynamodb_table" {
+  source = "../../"
+
+  create_table = false
+}
