c28f2029c20d1b5b4750226b52b68f675f940cab
vggonzal <9Tcostoamm>
2023-08-09 16:45:57 -0700

    implementing the lambda function

---
+++ terraform/hydrocron-db.tf
@@ -0,0 +1,44 @@
+module "dynamodb_table" {
+  source = "../../"
+
+  name                        = var.name_db
+  hash_key                    = "id"
+  range_key                   = "title"
+  table_class                 = "STANDARD"
+  deletion_protection_enabled = false
+
+  attributes = [
+    {
+      name = "id"
+      type = "N"
+    },
+    {
+      name = "title"
+      type = "S"
+    },
+    {
+      name = "feature_id"
+      type = "N"
+    }
+  ]
+
+  global_secondary_indexes = [
+    {
+      name               = "TitleIndex"
+      hash_key           = "title"
+      range_key          = "feature_id"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["id"]
+    }
+  ]
+
+  tags = {
+    Terraform   = "true"
+    Environment = "staging"
+  }
+}
+
+
+module "disabled_dynamodb_table" {
+  source = "../../"
+}
