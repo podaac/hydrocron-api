3e84f2d6178356b7af0a39a47da5249733836b25
vggonzal <9Tcostoamm>
2023-08-16 00:47:12 -0700

    dynamodb has data - testing

--- hydrocronapi/controllers/timeseries.py
+++ hydrocronapi/controllers/timeseries.py
@@ -90,8 +90,7 @@ def format_json(results: Generator, feature_id, elapsed_time):
         i = 0
 
         for res in results:
-            # feature_id
-            if str(res['feature_id']) == feature_id and res['feature_time'] != '-999999999999':  # and (res['width'] != '-999999999999')):
+            if res['feature_time'] != '-999999999999':  # and (res['width'] != '-999999999999')):
                 feature = {'properties': {}, 'geometry': {}, 'type': "Feature"}
                 feature['geometry']['coordinates'] = []
                 feature_type = ''
