61d1cc770b9e12557e49d0cc26881af7be221215
vggonzal <9Tcostoamm>
2023-09-27 18:20:53 -0700

    update lambda config

--- terraform/hydrocron-main.tf
+++ terraform/hydrocron-main.tf
@@ -20,42 +20,6 @@ data "aws_ssm_parameter" "hydrocron-db-sg" {
   name = "${local.hydrocrondb_resource_name}-sg"
 }
 
-#Security Groups
-
-## Application Lambda Security Group
-resource "aws_security_group" "service-app-sg" {
-  description = "controls access to the lambda Application"
-  vpc_id = var.vpc_id
-  name   = "${local.ec2_resources_name}-sg"
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-}
-
-## Allow ingress from the lambda security group to the database security group
-resource "aws_security_group_rule" "allow_app_in" {
-  type        = "ingress"
-  security_group_id = data.aws_ssm_parameter.hydrocron-db-sg.value
-  protocol    = "tcp"
-  from_port   = 3306
-  to_port     = 3306
-  source_security_group_id = aws_security_group.service-app-sg.id
-}
-
 
 resource "aws_api_gateway_deployment" "hydrocron-api-gateway-deployment-test" {
   rest_api_id = aws_api_gateway_rest_api.hydrocron-api-gateway-test.id
@@ -68,18 +32,14 @@ resource "aws_api_gateway_deployment" "hydrocron-api-gateway-deployment-test" {
   }
 }
 
-
-data "archive_file" "zip_the_python_code" {
-type        = "zip"
-source_dir  = "${path.module}/"
-output_path = "${path.module}/hydrocron.zip"
-}
-
 resource "aws_lambda_function" "hydrocron_api_lambda_timeseries_test" {
-  function_name = "${local.ec2_resources_name}-function"
-  role          = aws_iam_role.hydrocron-service-role.arn
-  filename      = "${path.module}/hydrocron-timeseries.zip"
+  function_name = "${local.ec2_resources_name}-function-timeseries-test"
+  filename = "${path.module}/../dist/${local.name}-${local.version}-test.zip"
+  source_code_hash = filebase64sha256("${path.module}/../dist/${local.name}-${local.version}-test.zip")
+  role          = aws_iam_role.hydrocron-service-role-test.arn
   timeout       = 5
+  handler       = "hydrocronapi.controllers.timeseries.lambda_handler"
+  runtime       = "python3.8"
 
   vpc_config {
     subnet_ids = var.private_subnets
@@ -97,12 +57,16 @@ resource "aws_lambda_function" "hydrocron_api_lambda_timeseries_test" {
 
   tags = var.default_tags
 }
+
 
 resource "aws_lambda_function" "hydrocron_api_lambda_subset_test" {
-  function_name = "${local.ec2_resources_name}-function"
-  role          = aws_iam_role.hydrocron-service-role.arn
-  filename      = "${path.module}/hydrocron-subset.zip"
+  function_name = "${local.ec2_resources_name}-function-subset-test"
+  filename = "${path.module}/../dist/${local.name}-${local.version}-test.zip"
+  source_code_hash = filebase64sha256("${path.module}/../dist/${local.name}-${local.version}-test.zip")
+  role          = aws_iam_role.hydrocron-service-role-test.arn
   timeout       = 5
+  handler       = "hydrocronapi.controllers.subset.lambda_handler"
+  runtime       = "python3.8"
 
   vpc_config {
     subnet_ids = var.private_subnets
@@ -120,6 +84,7 @@ resource "aws_lambda_function" "hydrocron_api_lambda_subset_test" {
 
   tags = var.default_tags
 }
+
 resource "aws_lambda_permission" "allow_hydrocron-timeseries-test" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
@@ -134,7 +99,7 @@ resource "aws_lambda_permission" "allow_hydrocron-timeseries-test" {
 resource "aws_lambda_permission" "allow_hydrocron-subset-test" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.subset.function_name
+  function_name = aws_lambda_function.hydrocron_api_lambda_subset_test.function_name
   principal     = "apigateway.amazonaws.com"
 
   # The "/*/*/*" portion grants access from any method on any resource
@@ -144,7 +109,7 @@ resource "aws_lambda_permission" "allow_hydrocron-subset-test" {
 
 
 
-# API Gateway
+#  API Gateway
 resource "aws_api_gateway_rest_api" "hydrocron-api-gateway-test" {
   name        = "${local.ec2_resources_name}-api-gateway-test"
   description = "API to access Hydrocron - test"
@@ -167,7 +132,7 @@ resource "aws_api_gateway_rest_api" "hydrocron-api-gateway-test" {
 }
 
 resource "aws_cloudwatch_log_group" "hydrocron-api-gateway-logs-test" {
-  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.hydrocron-api-gateway.id}/${aws_api_gateway_deployment.hydrocron-api-gateway-deployment-test.stage_name}"
+  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.hydrocron-api-gateway-test.id}/${aws_api_gateway_deployment.hydrocron-api-gateway-deployment-test.stage_name}"
   retention_in_days = 60
 }
 
@@ -175,174 +140,9 @@ output "url" {
   value = "${aws_api_gateway_deployment.hydrocron-api-gateway-deployment-test.invoke_url}/api"
 }
 
-resource "aws_ssm_parameter" "hydrocron-api-url" {
+resource "aws_ssm_parameter" "hydrocron-api-url-test" {
   name  = "hydrocron-api-url-test"
   type  = "String"
   value = aws_api_gateway_deployment.hydrocron-api-gateway-deployment-test.invoke_url
-}
-
-#########################
-# CodeBuild functionality
-#########################
-
-#CodeBuild IAM role
-
-resource "aws_iam_role" "hydrocron-codebuild-iam" {
-  name = "hydrocron-codebuild"
-
-  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/NGAPShRoleBoundary"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "codebuild.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "hydrocron-codebuild-policy" {
-  role = aws_iam_role.hydrocron-codebuild-iam.name
-
-
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "CloudWatchLogsPolicy",
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents",
-                "logs:GetLogEvents",
-                "ssm:GetParameters",
-                "ssm:GetParameter"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Sid": "CodeCommitPolicy",
-            "Effect": "Allow",
-            "Action": [
-                "codecommit:GitPull"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Sid": "S3GetObjectPolicy",
-            "Effect": "Allow",
-            "Action": [
-                "s3:GetObject",
-                "s3:GetObjectVersion",
-                "s3:List*"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Sid": "S3PutObjectPolicy",
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject"
-            ],
-            "Resource": [
-                "*"
-            ]
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateNetworkInterfacePermission",
-                "ec2:CreateNetworkInterface",
-                "ec2:DescribeDhcpOptions",
-                "ec2:DescribeNetworkInterfaces",
-                "ec2:DeleteNetworkInterface",
-                "ec2:DescribeSubnets",
-                "ec2:DescribeSecurityGroups",
-                "ec2:DescribeVpcs"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Resource": [
-                "arn:aws:codebuild:us-west-2:206226843404:project/*"
-            ],
-            "Action": [
-                "codebuild:StartBuild",
-                "codebuild:BatchGetBuilds",
-                "codebuild:BatchGetProjects"
-            ]
-        }
-    ]
-}
-POLICY
-}
-
-#CodeBuild Project
-
-resource "aws_codebuild_project" "hydrocron" {
-  name          = "Hydrocron"
-  description   = "Hydrocron Postman Testing"
-  build_timeout = "60"
-  service_role  = aws_iam_role.hydrocron-codebuild-iam.arn
-
-  artifacts {
-    packaging = "NONE"
-    name = "hydrocron-reports"
-    namespace_type = "BUILD_ID"
-    encryption_disabled = false
-    location = "podaac-services-${var.stage}-deploy"
-    path = "internal/hydrocron/test-reports"
-    type = "S3"
-  }
-
-  environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    image_pull_credentials_type = "CODEBUILD"
-    privileged_mode = false
-    image = "aws/codebuild/standard:3.0"
-    type = "LINUX_CONTAINER"
-  }
-
-  logs_config {
-    cloudwatch_logs {
-      status = "ENABLED"
-      group_name = "codeBuild"
-      stream_name = "Hydrocron"
-    }
-
-    s3_logs {
-      status = "DISABLED"
-    }
-  }
-
-  source {
-    insecure_ssl = false
-    type = "S3"
-    location = "podaac-services-${var.stage}-deploy/internal/hydrocron/"
-  }
-
-  vpc_config {
-    vpc_id = var.vpc_id
-
-    subnets = var.private_subnets
-
-    security_group_ids = [
-      aws_security_group.service-app-sg.id
-    ]
-  }
+  overwrite = true
 }
